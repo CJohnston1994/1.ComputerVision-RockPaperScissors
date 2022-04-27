da4f83648544766893ecdff90d5bbf1dfe3cf495
TheGlink <c.johnston1994@hotmail.co.uk>
2022-04-27 20:00:20 +0100

    Milestone 3 task completedd

--- RPS-Template.py
+++ RPS-Template.py
@@ -1,7 +1,7 @@
-import cv2
+import cv2 as cv2
 from keras.models import load_model
 import numpy as np
-model = load_model('YOUR_MODEL.h5')
+model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
