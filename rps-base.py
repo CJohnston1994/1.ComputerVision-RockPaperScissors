aa8601342e88bebbca74edad01e82b7a5a02bec4
TheGlink <c.johnston1994@hotmail.co.uk>
2022-04-27 20:01:28 +0100

    Stages comitted

---
+++ rps-base.py
@@ -0,0 +1,38 @@
+# function for  rock paper scissors
+import random
+
+options = ['Rock', 'Paper', 'Scissors']
+weakness = ['Paper', 'Scissors', 'Rock']
+
+def get_user_choice():
+    choice = input("Rock, paper or scissors? ")
+    #capitalize_Choice  = choice.capitalize()
+    if choice.capitalize() in options:
+        return choice.capitalize()
+    else:
+        print('please try again')
+        get_user_choice()
+
+def get_computer_choice():
+    roll = random.randint(0,2)
+    ai_choice = options[roll]
+    return ai_choice
+
+def get_Winner(user_Choice, computer_Choice):
+    if user_Choice == computer_Choice:
+        print("draw")
+    elif user_Choice == 'Rock' and computer_Choice == 'Paper':
+        print("You Lose... Try Again!")
+    elif user_Choice == 'Paper' and computer_Choice == 'Scissors':
+        print("You Lose... Try Again!")
+    elif user_Choice == 'Scissors' and computer_Choice == 'Rock':
+        print("You Lose... Try Again!")
+    else:
+        print('You Win')
+
+def play():
+    user_Choice = get_user_choice()
+    computer_Choice  = get_computer_choice()
+    get_Winner(user_Choice, computer_Choice)
+
+play()
